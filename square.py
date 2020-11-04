c88b04db2171392499010e762fc0a18f6f9b6019
Dago <squishysparrow7.7@gmail.com>
2020-11-05 10:44:33 +1300

    getting the files

--- square.py
+++ square.py
@@ -2,6 +2,6 @@ from PIL import Image, ImageDraw  # Pillow library, for all image handling
 from os import listdir  # To list files in a directory
 from os.path import isfile, join  # Check if path is an actual file and join 2 paths together
 
-to_convert = [f for f in listdir(r'.\input') if isfile(join(r'.\input', f))]
+to_convert = [join(r'.\input', f) for f in listdir(r'.\input') if isfile(join(r'.\input', f))]
 
 print(to_convert)
