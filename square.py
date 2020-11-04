cf714d6c22994b81e0fd21c6f403d40bc7e1059e
Dago <squishysparrow7.7@gmail.com>
2020-11-05 11:08:51 +1300

    canvas generating and comments on how pasting will work

--- square.py
+++ square.py
@@ -9,5 +9,18 @@ to_convert = [join(INPUT_PATH, f) for f in listdir(INPUT_PATH) if isfile(join(IN
 
 print(to_convert)
 
+transparent = [True if input('Y for transparent, N for blur: ').strip().lower() in ['y', 'yes'] else False]
+
 for image in to_convert:
-    Image.open(image)
+    old_img = Image.open(image)
+
+    x, y = old_img.size
+
+    big_side, small_side = max([x, y]), min([x,y])
+
+    new_image = Image.new('RGBA', (big_side, big_side), (0, 0, 0, 0))
+
+    # To get where to paste the image:
+    #   Coords are from top left to bottom right
+    #   Biggest side's coords are 0 so it stays in line with the new canvas
+    #   new side's coords are (biggest_side-small_side)/2, gets the difference and splits it so the image is at the middle
