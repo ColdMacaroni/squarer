d7bb53e4d5dd2604aa512a7872c9b1ff8c15443f
Dago <squishysparrow7.7@gmail.com>
2020-11-05 11:17:08 +1300

    added saving stuff

--- square.py
+++ square.py
@@ -5,22 +5,33 @@ from os.path import isfile, join  # Check if path is an actual file and join 2 p
 INPUT_PATH = r'.\input'
 OUTPUT_PATH = r'.\output'
 
-to_convert = [join(INPUT_PATH, f) for f in listdir(INPUT_PATH) if isfile(join(INPUT_PATH, f))]
+to_convert = [f for f in listdir(INPUT_PATH) if isfile(join(INPUT_PATH, f))]
 
 print(to_convert)
 
 transparent = [True if input('Y for transparent, N for blur: ').strip().lower() in ['y', 'yes'] else False]
 
 for image in to_convert:
-    old_img = Image.open(image)
+    old_img = Image.open(join(INPUT_PATH, image))
 
-    x, y = old_img.size
+    x, y = old_img.size  # Get size in pixels
 
-    big_side, small_side = max([x, y]), min([x,y])
+    big_side, small_side = max([x, y]), min([x,y]) # Get biggest and smallest side for creating the square
 
-    new_image = Image.new('RGBA', (big_side, big_side), (0, 0, 0, 0))
+    new_image = Image.new('RGBA', (big_side, big_side), (0, 0, 0, 0)) # Square canvas. Size lenght of biggest_side
 
     # To get where to paste the image:
     #   Coords are from top left to bottom right
     #   Biggest side's coords are 0 so it stays in line with the new canvas
     #   new side's coords are (biggest_side-small_side)/2, gets the difference and splits it so the image is at the middle
+
+    new_image.paste(old_img)
+
+    # Get rid of extension
+    # Allows for filenames to have . in them
+    filename = image.split('.')
+    del filename[-1]
+    '.'.join(filename)
+
+    # Save with the new extension
+    new_image.save(filename + '.png')
