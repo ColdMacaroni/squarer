73948952d9adbc030f937493bad68712bfa7bdf8
Dago <squishysparrow7.7@gmail.com>
2020-11-05 10:50:06 +1300

    opening imgs

--- square.py
+++ square.py
@@ -2,6 +2,12 @@ from PIL import Image, ImageDraw  # Pillow library, for all image handling
 from os import listdir  # To list files in a directory
 from os.path import isfile, join  # Check if path is an actual file and join 2 paths together
 
-to_convert = [join(r'.\input', f) for f in listdir(r'.\input') if isfile(join(r'.\input', f))]
+INPUT_PATH = r'.\input'
+OUTPUT_PATH = r'.\output'
+
+to_convert = [join(INPUT_PATH, f) for f in listdir(INPUT_PATH) if isfile(join(INPUT_PATH, f))]
 
 print(to_convert)
+
+for image in to_convert:
+    Image.open(image)
